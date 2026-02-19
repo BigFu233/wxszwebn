fcbd0e189c4b3883d85fb40cc51f4d8f904a234f
BigFu233 <139222134+BigFu233@users.noreply.github.com>
2026-02-19 15:49:35 +0800

    Update request.ts

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const service = axios.create({
   baseURL: import.meta.env.VITE_API_URL || '/.netlify/functions/proxy',
-  timeout: 5000
+  timeout: 20000
 });
 
 service.interceptors.request.use(
